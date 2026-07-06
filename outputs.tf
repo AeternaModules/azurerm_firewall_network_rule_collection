04917270c811a9fd01e25e67f5ea084940516575
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "firewall_network_rule_collections" {
   description = "All firewall_network_rule_collection resources"
   value       = azurerm_firewall_network_rule_collection.firewall_network_rule_collections
 }
+output "firewall_network_rule_collections_action" {
+  description = "List of action values across all firewall_network_rule_collections"
+  value       = [for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : v.action]
+}
+output "firewall_network_rule_collections_azure_firewall_name" {
+  description = "List of azure_firewall_name values across all firewall_network_rule_collections"
+  value       = [for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : v.azure_firewall_name]
+}
+output "firewall_network_rule_collections_name" {
+  description = "List of name values across all firewall_network_rule_collections"
+  value       = [for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : v.name]
+}
+output "firewall_network_rule_collections_priority" {
+  description = "List of priority values across all firewall_network_rule_collections"
+  value       = [for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : v.priority]
+}
+output "firewall_network_rule_collections_resource_group_name" {
+  description = "List of resource_group_name values across all firewall_network_rule_collections"
+  value       = [for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : v.resource_group_name]
+}
+output "firewall_network_rule_collections_rule" {
+  description = "List of rule values across all firewall_network_rule_collections"
+  value       = [for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : v.rule]
+}
 
