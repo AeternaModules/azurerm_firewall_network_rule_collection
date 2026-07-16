33c65de144c913d9176b9ce3ecd0ffb80ca826ae
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "firewall_network_rule_collections_id" {
   description = "Map of id values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.id }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "firewall_network_rule_collections_action" {
   description = "Map of action values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.action }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.action if v.action != null && length(v.action) > 0 }
 }
 output "firewall_network_rule_collections_azure_firewall_name" {
   description = "Map of azure_firewall_name values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.azure_firewall_name }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.azure_firewall_name if v.azure_firewall_name != null && length(v.azure_firewall_name) > 0 }
 }
 output "firewall_network_rule_collections_name" {
   description = "Map of name values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.name }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "firewall_network_rule_collections_priority" {
   description = "Map of priority values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.priority }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.priority if v.priority != null }
 }
 output "firewall_network_rule_collections_resource_group_name" {
   description = "Map of resource_group_name values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.resource_group_name }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "firewall_network_rule_collections_rule" {
   description = "Map of rule values across all firewall_network_rule_collections, keyed the same as var.firewall_network_rule_collections"
-  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.rule }
+  value       = { for k, v in azurerm_firewall_network_rule_collection.firewall_network_rule_collections : k => v.rule if v.rule != null && length(v.rule) > 0 }
 }
 
